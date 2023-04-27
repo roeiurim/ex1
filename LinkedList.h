6ab2f74ed5744b1895a869add1265b33b9bcb32c
Roei <roei.urim@campus.technion.ac.il>
2023-04-28 02:22:37 +0300

    replaced pointer to struct parameters with generic typdef'd datatype

--- LinkedList.h
+++ LinkedList.h
@@ -2,13 +2,14 @@
 #define LINKED_LIST_H
 
 typedef struct node *Node;
+typedef void* dataType;
 
 /*create new Node object with passed data. return created Node object*/
-Node createNode(struct data*);
+Node createNode(dataType);
 
 /*create new Node object with passed data. insert created Node at the start of the list
 head points to. return new head (created Node)*/
-Node insertAtStart(Node head, struct data*);
+Node insertAtStart(Node head, dataType);
 
 /*create copy of list pointed to by input head. return head Node copy*/
 Node cloneList(Node head);
